7edf99844eebcb30ec3dcbdb19e614d07a775cd1
leechanx <leechan8@outlook.com>
2018-06-25 22:05:06 +0800

    移动main

--- agent/main.go
+++ agent/main.go
@@ -14,7 +14,7 @@ import (
 )
 
 func loadSelfConfig() {
-	cfgPath := flag.String("conf", "../conf/easy-config-agent.ini", "configure file path")
+	cfgPath := flag.String("conf", "conf/easy-config-agent.ini", "configure file path")
 	flag.Parse()
 	//加载配置文件
 	cfg, err := config.ReadDefault(*cfgPath)
